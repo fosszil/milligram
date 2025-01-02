1728f0d56161d8a04835ae0e3a450a6212f222d2
aaqil <kidobop@gmail.com>
2025-01-02 23:52:04 +0530

    added mutation observer

--- content.js
+++ content.js
@@ -1,3 +1,17 @@
 let reelCount = 0;
 
-var target = document.querySelector(div[role="presentation"])
+const target = document.querySelector(div[role="presentation"])
+const config = { attributes:true,childList:true,subtree:true};
+
+const callback = (mutationList,observer) => {
+    for (const mutation of mutationList) {
+        if(mutation.type == "childList"){
+            console.log("child list has been added or removed");
+        } else if (mutation.type == "attributes") {
+            console.log("Attributes have been changed");
+        }
+    }
+};
+
+const observer = new MutationObserver(callback);
+observer.observe(target,config);
