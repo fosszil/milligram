3ff2542316e83acddaed6d8a6417d051a8874606
aaqil <kidobop@gmail.com>
2025-05-16 00:35:08 +0530

    fixed the shorts limiting feature
    the issue was with the manifest version

--- popup.js
+++ popup.js
@@ -22,3 +22,44 @@ chrome.tabs.query(
     });
   }
 );
+
+
+document.addEventListener('DOMContentLoaded',()=>{
+  const saveBtn = document.getElementById("saveBtn");
+  const shortsLimitInput = document.getElementById("shortsLimit");
+  const statusElement = document.getElementById("status");
+
+  chrome.storage.sync.get(['shortsLimit'],(result)=>{
+    if(result.shortsLimit !== undefined){
+      shortsLimitInput.value = result.shortsLimit;
+    }
+  });
+
+  saveBtn.addEventListener("click",()=>{
+    const limit = parseInt(shortsLimitInput.value);
+
+    if (isNaN(limit)) {
+      statusElement.textContent = "Please enter a valid number.";
+      return;
+    }
+
+    chrome.storage.sync.set({shortsLimit:limit},()=>{
+      statusElement.textContent = "Limit saved";
+
+    chome.tabs.query({active:true, currentWindow: true},(tabs)=>{
+      if(tabs[0]){
+        chrome.tabs.sendMessage(tabs[0].id,{
+          type: "START_LIMIT_CHECK",
+          shortsLimit: limit
+        });
+      }
+    });
+
+    setTimeout(() => {
+      statusElement.textContent = "";
+    }, 2000);
+
+    })
+
+  });
+})
